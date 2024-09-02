de3394327a86eeb438b5ba31641e3e47d4d0c51b
zakariyasalie <zakariya.salie07@gmail.com>
2024-09-02 14:11:56 +0200

    config

---
+++ config/config.js
@@ -0,0 +1,13 @@
+// import {pool} from '../config/config.js'
+import {createPool} from 'mysql2/promise'
+import {config} from 'dotenv'
+config()
+
+const pool=createPool({
+host:process.env.HOST,
+user:process.env.USER,
+password:process.env.PASSWORD,
+database: process.env.DATABASE
+
+})
+export default pool;
